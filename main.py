b2cc73174f7f7197c45f0d85d43fb24f95c7eb58
iMaary <m.rodriguesnave@gmail.com>
2024-10-02 00:42:05 +0100

    add PoC do make decision

--- main.py
+++ main.py
@@ -1,65 +1,16 @@
 import random
+import time
 
-class CommonAgent:
-    def __init__(self, local_id: int, name: str, trust_level: float):
+class ConsumerAgent():
+    def __init__(self, local_id: int, name, budget, initial_usage=0):
         self.name = name
-        self.trust_level = trust_level
-        self.alpha = 0.01
-        self.beta = 0.08
-        # On the furute these vars (gammas, significance, trend) should be setted by:
-        # PRODUCER: seasonality based on its energy type and avaliability.
-        # CONSUMER: consumer payment historic
-        self.gammas = []
-        self.significance = None
-        self.trend = None
-        # On the furute we can add the historic of failures and trust level to update that.
-        self.failure_prob = None
-
-    def update_trust_level(self, failed=False):
-        if failed:
-            self.trust_level = self.trust_level*(1 - self.beta)
-        else:
-            self.trust_level = self.trust_level*(1 + self.alpha)
-
-    def set_alpha_beta(gammas=[]):
-        self.alpha = gammas[0]*significance + gammas[1]*trend
-        self.beta = gammas[2]*significance + gammas[3]*trend
-    
-    def isOperationFailed(self, failure_prob=0.0):
-        """
-            Function: decide if the operation is failed or not
-        """
-        if random.random() < failure_prob / 100:
-            return True
-        else:
-            return False
-
-
-class ConsumerAgent(CommonAgent):
-    def __init__(self, name, budget, initial_usage=0):
-        super().__init__(name=name, trust_level=1)
         self.budget = budget
         self.usage = initial_usage
-    
-    def consume_electricity(self, amount):
-        """
-            Function: consume electricty based on the best tradeoff 
-            (make decision).
-            -
-            There is a test for simulate the failed operation.
-        """
-        if not self.isOperationFailed():
-            self.make_decision()
-            self.update_trust_level()
-            return True
-        else:
-            self.update_trust_level(failed=True)
-            return False
 
     def calculate_cost(self, price_per_unit):
         return self.usage * price_per_unit
 
-    def make_decision(producers=[], current_price_per_unit, max_acceptable_price):
+    def make_decision(self, producers=[]):
         """
             Choose "Producer" based on price (unit_cost) and trust.
         """
@@ -70,8 +21,8 @@ class ConsumerAgent(CommonAgent):
             # Adjust decision based on trust level
             if prod.trust_level >= 0.5:
                 # If trust level is high or medium, make decision as usual
-                cost = self.calculate_cost(current_price_per_unit)
-                if cost <= self.budget and current_price_per_unit <= max_acceptable_price:
+                cost = self.calculate_cost(prod.unit_cost)
+                if cost <= self.budget and prod.produce_electricity(self.usage):
                     producer_scores[prod.name] = prod.get_score()
                 else:
                     """Reduce Usage""" 
@@ -79,19 +30,33 @@ class ConsumerAgent(CommonAgent):
                 # If trust level is low, reduce usage regardless of cost
                 """Reduce Usage"""
 
-        max_score_producer_name = max(producer_scores, key=producer_scores.get)
+        if(producer_scores):
+            max_score_producer_name = max(producer_scores, key=producer_scores.get)
+        else:
+            max_score_producer_name = "None producer avaliable."
 
         return max_score_producer_name
     
 
-class ProducerAgent(CommonAgent):
-    def __init__(self, name, unit_cost, energy_type, initial_capacity=0):
-        super().__init__(name=name, trust_level=1)
+class ProducerAgent:
+    def __init__(self, local_id: int, name, initial_trust_level, unit_cost, initial_capacity=0, energy_type=1, failure_prob=0):
+        self.name = name
+        self.trust_level = initial_trust_level
         self.unit_cost = unit_cost
         self.capacity = initial_capacity
         self.energy_type = energy_type
+        self.alpha = 0.01
+        self.beta = 0.08
+        # On the furute we can add the historic of failures and trust level to update that.
+        self.failure_prob = failure_prob
         # measured by the seasonality based on its energy type and avaliability. 
-        self.quality = None
+        self.quality = 0.9  # Initial quality rating
+        # On the furute these vars (gammas, significance, trend) should be setted by:
+        # PRODUCER: seasonality based on its energy type and avaliability.
+        # CONSUMER: consumer payment historic
+        self.gammas = []
+        self.significance = None
+        self.trend = None
 
     def produce_electricity(self, amount):
         """
@@ -106,8 +71,47 @@ class ProducerAgent(CommonAgent):
             self.update_trust_level()
             return True
         else:
+            self.update_trust_level(failed=True)
             return False
 
-    def get_score():
+    def get_score(self):
         return self.unit_cost*(1+0.001-self.trust_level)
 
+    def update_trust_level(self, failed=False):
+        if failed:
+            self.trust_level = self.trust_level*(1 - self.beta)
+        else:
+            self.trust_level = min(self.trust_level*(1 + self.alpha), 1)
+
+    def set_alpha_beta(gammas=[]):
+        self.alpha = gammas[0]*significance + gammas[1]*trend
+        self.beta = gammas[2]*significance + gammas[3]*trend
+    
+    def isOperationFailed(self):
+        """
+            Function: decide if the operation is failed or not
+        """
+        if random.random() < self.failure_prob:
+            return True
+        else:
+            return False
+
+    def print_status(self):
+        space_fmt = " "*(20 - len(self.name))
+        print(f"{self.name}{space_fmt}| TRUST LEVEL: {self.trust_level} - CAPACITY: {self.capacity}")
+
+
+p1 = ProducerAgent(111, "Eólica", 0.8, 12, 1200, 1, 0.2)
+p2 = ProducerAgent(222, "Solar", 0.75, 6, 600, 2, 0.15)
+p3 = ProducerAgent(333, "Hidroelétrica", 0.9, 17, 1700, 3, 0.1)
+
+c1 = ConsumerAgent(123, "Genivaldo", 5000, 9)
+
+while(True):
+    p1.print_status()
+    p2.print_status()
+    p3.print_status()
+
+    print(c1.make_decision([p1,p2,p3]))
+
+    time.sleep(2)
