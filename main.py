9a6f8941f3349ac7a0dd5f3705cf9c97f9aa5867
iMaary <m.rodriguesnave@gmail.com>
2024-10-02 01:40:16 +0100

    add repast4py     * TODO: get producers info or object

--- main.py
+++ main.py
@@ -1,8 +1,18 @@
 import random
 import time
 
-class ConsumerAgent():
-    def __init__(self, local_id: int, name, budget, initial_usage=0):
+from mpi4py import MPI
+from typing import Tuple
+
+from repast4py import core, random, space, schedule, logging, parameters
+from repast4py import context as ctx
+import repast4py
+
+class ConsumerAgent(core.Agent):
+    TYPE=0
+
+    def __init__(self, local_id: int, rank: int, name, budget, initial_usage=0):
+        super().__init__(id=local_id, type=ConsumerAgent.TYPE, rank=rank)
         self.name = name
         self.budget = budget
         self.usage = initial_usage
@@ -35,11 +45,18 @@ class ConsumerAgent():
         else:
             max_score_producer_name = "None producer avaliable."
 
+        print(f"Choosen Producer: {max_score_producer_name}")
         return max_score_producer_name
+
+    def save(self) -> Tuple:
+        return (self.uid, self.budget)
     
 
-class ProducerAgent:
-    def __init__(self, local_id: int, name, initial_trust_level, unit_cost, initial_capacity=0, energy_type=1, failure_prob=0):
+class ProducerAgent(core.Agent):
+    TYPE=1
+
+    def __init__(self, local_id: int, rank: int, name, initial_trust_level, unit_cost, initial_capacity=0, energy_type=1, failure_prob=0):
+        super().__init__(id=local_id, type=ProducerAgent.TYPE, rank=rank)
         self.name = name
         self.trust_level = initial_trust_level
         self.unit_cost = unit_cost
@@ -79,9 +96,10 @@ class ProducerAgent:
 
     def update_trust_level(self, failed=False):
         if failed:
-            self.trust_level = self.trust_level*(1 - self.beta)
+            self.trust_level = max(self.trust_level*(1 - self.beta), 0)
         else:
-            self.trust_level = min(self.trust_level*(1 + self.alpha), 1)
+            self.trust_level = min(self.trust_level*(1 + self.alpha), 1)  
+        self.print_status()
 
     def set_alpha_beta(gammas=[]):
         self.alpha = gammas[0]*significance + gammas[1]*trend
@@ -100,18 +118,47 @@ class ProducerAgent:
         space_fmt = " "*(20 - len(self.name))
         print(f"{self.name}{space_fmt}| TRUST LEVEL: {self.trust_level} - CAPACITY: {self.capacity}")
 
+    def save(self) -> Tuple:
+        return (self.uid, self.trust_level)
 
-p1 = ProducerAgent(111, "Eólica", 0.8, 12, 1200, 1, 0.2)
-p2 = ProducerAgent(222, "Solar", 0.75, 6, 600, 2, 0.15)
-p3 = ProducerAgent(333, "Hidroelétrica", 0.9, 17, 1700, 3, 0.1)
 
-c1 = ConsumerAgent(123, "Genivaldo", 5000, 9)
+class Model:
+    def __init__(self, comm: MPI.Intracomm):
+        self.runner = schedule.init_schedule_runner(comm)
+        self.runner.schedule_repeating_event(1, 1, self.handle_agent)
+        self.runner.schedule_stop(20)
 
-while(True):
-    p1.print_status()
-    p2.print_status()
-    p3.print_status()
+        # create the context to hold the agents and manage cross process synchronization
+        self.context = ctx.SharedContext(comm)
 
-    print(c1.make_decision([p1,p2,p3]))
+        rank = comm.Get_rank()
+        if rank == 0:
+            agent_c1 = ConsumerAgent(123, rank, "Genivaldo", 5000, 9)
+            self.context.add(agent_c1)
+        elif rank == 1:
+            p1 = ProducerAgent(111, rank, "Eólica", 0.8, 12, 1200, 1, 0.2)
+            self.context.add(p1)
+            p2 = ProducerAgent(222, rank, "Solar", 0.75, 6, 600, 2, 0.15)
+            self.context.add(p2)
+            p3 = ProducerAgent(333, rank, "Hidroelétrica", 0.9, 17, 1700, 3, 0.1)
+            self.context.add(p3)
 
-    time.sleep(2)
+    def handle_agent(self):
+        producers = []
+    
+        for agent in self.context.agents():
+            if agent.type == 0:
+                agent.make_decision(producers)
+
+def main():
+    comm = MPI.COMM_WORLD
+    # id = comm.Get_rank()                    #number of the process running the code
+    # numProcesses = comm.Get_size()          #total number of processes running
+    # myHostName = MPI.Get_processor_name()   #machine name running the code
+
+    model = Model(comm)
+    model.runner.execute()
+
+main()
+
+# mpirun -n 2 python3 main.py
